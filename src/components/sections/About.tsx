d5fadbe640b45aeda69a7174624d276a56deeef9
Broadway The Lyricist <ramcorpmanagement@gmail.com>
2026-09-26 15:12:00 -0400

    Separate portraits from background effects

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -59,7 +59,7 @@ export default function About() {
       <div className="absolute inset-0 -z-20 bg-gradient-to-l from-bg/84 via-bg/42 to-transparent" />
       <div className="absolute inset-0 -z-20 bg-gradient-to-t from-bg/48 via-transparent to-bg/10" />
 
-      <div aria-hidden="true" className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden lg:w-[56%]">
+      <div aria-hidden="true" className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden lg:w-[52%]">
         <div
           ref={subjectRef}
           className="about-subject absolute bottom-0 -left-[42vw] w-[154vw] sm:-left-[30vw] sm:w-[132vw] lg:-left-[18vw] lg:w-[88vw] lg:max-w-[1690px]"
@@ -70,10 +70,9 @@ export default function About() {
             width={2048}
             height={1152}
             sizes="(max-width: 640px) 154vw, (max-width: 1024px) 132vw, 88vw"
-            className="block h-auto w-full"
+            className="about-subject-image block h-auto w-full"
           />
         </div>
-        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-[18%] bg-gradient-to-b from-transparent via-bg/35 to-bg" />
       </div>
 
       <div className="relative mx-auto max-w-7xl px-5 sm:px-8">
